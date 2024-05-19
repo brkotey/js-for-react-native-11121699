0dbe02029d6d82a62997f8662b65b111ed157a67
brkotey <brkotey@st.ug.edu.gh>
2024-05-19 20:49:54 +0100

    Task 2 completed

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -13,3 +13,26 @@ function processArray(arr) {
     let inputArray = [8, 20, 28, 30, 16]; 
     let outputArray = processArray(inputArray); 
     console.log(outputArray);
+
+    function formatArrayStrings(Strings, Numbers) { 
+        const formattedStrings = []; 
+        for (let i = 0; i < Strings.length; i++) { 
+          const string = Strings[i]; 
+          const number = Numbers[i]; 
+          if (number % 2 === 0) { 
+            formattedStrings.push(string.toUpperCase()); 
+          } else { 
+            formattedStrings.push(string.toLowerCase()); 
+          } 
+        } 
+        return formattedStrings; 
+      } 
+       
+     
+    const originalStrings = ["The”,  “Boy”, “Is”, “Awesome"]; 
+    const processedNumbers = [8, 20, 28, 30, 16];  
+     
+    const formattedStrings = formatArrayStrings(originalStrings, processedNumbers); 
+     
+    console.log("Original Strings:", originalStrings); 
+    console.log("Formatted Strings:", formattedStrings);
